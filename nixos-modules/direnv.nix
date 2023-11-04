836283e33f77623f5c06dfd002c4147893512b62
Yang, Bo <bo@preemo.io>
2023-11-04 16:00:00 +0000

    Update nixpkgs to https://github.com/NixOS/nixpkgs/pull/265481 to support NVMe and UEFI

--- nixos-modules/direnv.nix
+++ nixos-modules/direnv.nix
@@ -1,17 +1,3 @@
 { pkgs, lib, ... }: {
-  environment.systemPackages = [ pkgs.direnv pkgs.nix-direnv ];
-  nix.settings = {
-    keep-outputs = true;
-    keep-derivations = true;
-  };
-  environment.pathsToLink = [
-    "/share/nix-direnv"
-  ];
-  nixpkgs.overlays = [
-    (self: super: { nix-direnv = super.nix-direnv.override { enableFlakes = true; }; })
-  ];
-  programs.bash.interactiveShellInit = lib.mkAfter ''
-    eval "$(direnv hook bash)"
-  '';
-
+  programs.direnv.enable = true;
 }
