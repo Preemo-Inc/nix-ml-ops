a479a20da088b856998102d37eb839f2db24437e
Yang, Bo <bo@preemo.io>
2023-11-11 03:27:27 +0000

    Fix libstdc++ workaround

--- flake-modules/devenv-python-with-libstdc++.nix
+++ flake-modules/devenv-python-with-libstdc++.nix
@@ -10,13 +10,16 @@ topLevel@{ flake-parts-lib, lib, inputs, ... }: {
     options.perSystem = flake-parts-lib.mkPerSystemOption
       (perSystem@{ pkgs, system, ... }: {
         ml-ops.common.pythonPackage = pythonPackage: {
-          overrideAttrs = lib.mkIf pythonPackage.config.base-package.stdenv.isLinux [
-            (self: super: {
-              env = super.env // {
-                # Link libstdc++ to python interpreter so that packages in manylinux ABI can find it out-of-the-box without LD_LIBRARY_PATH
-                # TODO: Add more libraries here when encountering an ImportError
-                NIX_LDFLAGS = "--no-as-needed -lstdc++ --as-needed ${super.env.NIX_LDFLAGS}";
-              };
+          override = [
+            (old: lib.attrsets.optionalAttrs old.stdenv.isLinux {
+              self = old.self.overrideAttrs
+                (self: super: {
+                  env = super.env // {
+                    # Link libstdc++ to python interpreter so that packages in manylinux ABI can find it out-of-the-box without LD_LIBRARY_PATH
+                    # TODO: Add more libraries here when encountering an ImportError
+                    NIX_LDFLAGS = "--no-as-needed -lstdc++ --as-needed ${super.env.NIX_LDFLAGS}";
+                  };
+                });
             })
           ];
         };
