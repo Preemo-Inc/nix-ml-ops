fd53e49b292a828e101f75fabf07fc769db89824
Yang, Bo <bo@preemo.io>
2023-11-28 15:09:27 -0800

    Update comments for example projects' inputs

--- examples/devenv-python/flake.nix
+++ examples/devenv-python/flake.nix
@@ -1,11 +1,23 @@
 {
   inputs = {
     call-flake.url = "github:divnix/call-flake";
+
+    # We don't want to lock the `nix-ml-ops` flake here, because we want to
+    # use the latest version of `nix-ml-ops` in this example project.
+    # 
+    # For a real project, you should lock the `nix-ml-ops` flake by uncommenting
+    # the following line.
+    # nix-ml-ops.url = "github:Preemo-Inc/nix-ml-ops";
   };
   outputs =
     lockedInputs:
     let
       unlockedInputs = {
+        # In this example project, nix-ml-ops is loaded from a relative path,
+        # so that it would not be included in the lock file.
+        #
+        # For a real project, `unlockedInputs` should be removed in favor of
+        # locked inputs.
         nix-ml-ops = lockedInputs.call-flake ./../..;
       };
       inputs = unlockedInputs // lockedInputs;
